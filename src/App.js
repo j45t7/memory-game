e8def3c8681f456e09e90b02882d0910da766b17
Justyna Marciniak <justy.marciniak@gmail.com>
2021-11-23 12:11:37 +0900

    add flipping card logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { useState, useEffect, useCallback } from 'react'
 import './App.css'
+import Card from './components/Card'
 
 const URL = 'https://pokeapi.co/api/v2/pokemon'
 
 function App() {
   // console.log(getPokemonData())
   const [pokemons, setPokemons] = useState([])
+  // const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
 
   const getPokemonData = useCallback(async () => {
     const allPokemonData = new Array(200)
@@ -27,6 +33,7 @@ function App() {
           image: `${data.sprites.front_default}`,
           type: data.types[0].type.name,
           pokeId: Math.random(),
+          matched: false,
         }
         pokeData.push(transformedPokemon)
       }
@@ -39,16 +46,66 @@ function App() {
     getPokemonData()
   }, [getPokemonData])
 
+  const handleChoice = (pokemon) => {
+    choiceOne ? setChoiceTwo(pokemon) : setChoiceOne(pokemon)
+  }
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+      if (choiceOne.image === choiceTwo.image) {
+        console.log('cards match')
+        setPokemons((prevCards) => {
+          return prevCards.map((pokemon) => {
+            if (pokemon.image === choiceOne.image) {
+              return { ...pokemon, matched: true }
+            } else {
+              return pokemon
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        console.log("cards don't match")
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  //reset choces and increase turn counter
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns((prevTurn) => prevTurn + 1)
+    setDisabled(false)
+  }
+
+  //shuffle cards
+  // const shuffleCards = () => {
+  //   const shuffledCards = [...cardImages, ...cardImages]
+  //     .sort(() => Math.random() - 0.5)
+  //     .map((card) => ({ ...card, id: Math.random() }))
+  //   setChoiceOne(null)
+  //   setChoiceTwo(null)
+  //   setTurns(0)
+  //   setCards(shuffledCards)
+  // }
+
+  // useEffect(() => {
+  //   shuffleCards()
+  // }, [])
   return (
     <div className='App'>
       <h1>Pokemon Memory Game</h1>
       <div className='deck'>
         {pokemons.map((poke) => (
-          <div className='card' key={poke.pokeId}>
-            <img src={poke.image} alt='' />
-            <p className='card-name'>{poke.name}</p>
-            <p className='card-type'>{poke.type}</p>
-          </div>
+          <Card
+            key={poke.pokeId}
+            poke={poke}
+            handleChoice={handleChoice}
+            flipped={poke === choiceOne || poke === choiceTwo || poke.matched}
+            disabled={disabled}
+          />
         ))}
       </div>
     </div>
