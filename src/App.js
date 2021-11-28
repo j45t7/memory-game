e0c179ed262d32b2b59dd3a6019a60df897d7007
Justyna Marciniak <justy.marciniak@gmail.com>
2021-11-28 19:13:21 +0900

    add loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { useState, useEffect, useCallback } from 'react'
 import './App.css'
 import Card from './components/Card'
+import Moves from './components/Moves'
+import SelectLevel from './components/SelectLevel'
+import Title from './components/Title'
 
 const URL = 'https://pokeapi.co/api/v2/pokemon'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
   const [pokemons, setPokemons] = useState([])
   const [level, setLevel] = useState('8')
   const [moves, setMoves] = useState(0)
@@ -13,18 +18,22 @@ function App() {
   const [disabled, setDisabled] = useState(false)
 
   const getPokemonData = useCallback(async () => {
-    const allPokemonData = new Array(200)
-      .fill(null)
-      .map((id, idx) => idx + 1)
-      .sort(() => Math.random() - 0.5)
-    const setOfPokemons = allPokemonData.slice(0, level)
-    const finalSetPokemons = [...setOfPokemons, ...setOfPokemons].sort(
-      () => Math.random() - 0.5
-    )
-    const pokeData = []
-    for (let pokeId of finalSetPokemons) {
-      const response = await fetch(`${URL}/${pokeId}`)
-      if (response.ok) {
+    try {
+      const allPokemonData = new Array(200)
+        .fill(null)
+        .map((id, idx) => idx + 1)
+        .sort(() => Math.random() - 0.5)
+      const setOfPokemons = allPokemonData.slice(0, level)
+      const finalSetPokemons = [...setOfPokemons, ...setOfPokemons].sort(
+        () => Math.random() - 0.5
+      )
+      const pokeData = []
+      for (let pokeId of finalSetPokemons) {
+        const response = await fetch(`${URL}/${pokeId}`)
+        if (!response.ok) {
+          throw new Error('Something went wrong!')
+        }
+
         const data = await response.json()
         const transformedPokemon = {
           id: data.id,
@@ -36,11 +45,14 @@ function App() {
         }
         pokeData.push(transformedPokemon)
       }
+
+      setPokemons(pokeData)
+    } catch (error) {
+      setError(error.message)
     }
-    setPokemons(pokeData)
+    setIsLoading(false)
   }, [level])
 
-  console.log(pokemons)
   useEffect(() => {
     getPokemonData()
   }, [getPokemonData])
@@ -71,7 +83,7 @@ function App() {
     }
   }, [choiceOne, choiceTwo])
 
-  //reset choces and increase turn counter
+  //reset choices and increase turn counter
   const resetTurn = () => {
     setChoiceOne(null)
     setChoiceTwo(null)
@@ -80,66 +92,39 @@ function App() {
   }
 
   const handleChange = (event) => {
-    // event.preventDefault()
-    console.log(event.target.value)
+    event.preventDefault()
     setLevel(event.target.value)
+  }
+
+  let content = (
+    <div className={level === '10' ? 'deck--five' : 'deck--four'}>
+      {pokemons.map((pokemon) => (
+        <Card
+          key={pokemon.pokeId}
+          pokemon={pokemon}
+          handleChoice={handleChoice}
+          flipped={
+            pokemon === choiceOne || pokemon === choiceTwo || pokemon.matched
+          }
+          disabled={disabled}
+        />
+      ))}
+    </div>
+  )
+  if (error) {
+    content = <p>{error}</p>
+  }
+
+  if (isLoading) {
+    content = <p className='loading'>Catching them all...</p>
   }
 
   return (
     <div className='App'>
-      <span>
-        <img src='img/pokemon.png' className='title__image' alt='pokemon' />
-        <h1 className='title'>Memory Game</h1>
-      </span>
-      <form className='level'>
-        <label htmlFor='' className='level__label'>
-          <input
-            className='level__input'
-            type='radio'
-            name='easy'
-            value='4'
-            checked={level === '4'}
-            onChange={handleChange}
-          />
-          Easy
-        </label>
-        <label htmlFor='' className='level__label'>
-          <input
-            className='level__input'
-            type='radio'
-            name='medium'
-            value='8'
-            checked={level === '8'}
-            onChange={handleChange}
-          />
-          Medium
-        </label>
-        <label htmlFor='' className='level__label'>
-          <input
-            className='level__input'
-            type='radio'
-            name='hard'
-            value='10'
-            checked={level === '10'}
-            onChange={handleChange}
-          />
-          Hard
-        </label>
-      </form>
-      <div className={level === '10' ? 'deck--five' : 'deck--four'}>
-        {pokemons.map((pokemon) => (
-          <Card
-            key={pokemon.pokeId}
-            pokemon={pokemon}
-            handleChoice={handleChoice}
-            flipped={
-              pokemon === choiceOne || pokemon === choiceTwo || pokemon.matched
-            }
-            disabled={disabled}
-          />
-        ))}
-      </div>
-      <p className='moves'>Moves: {moves}</p>
+      <Title />
+      <SelectLevel level={level} handleChange={handleChange} />
+      {content}
+      <Moves moves={moves} />
     </div>
   )
 }
